867f9e6a89ffb556a1342f8244f4dd46b0c18780
patwu22 <wu.patrick19@gmail.com>
2024-11-20 16:08:09 -0500

    update test cases

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,27 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import {render, screen, fireEvent} from '@testing-library/react'
+import Counter from '../components/Counter'
 
 beforeEach(() => {
-  // Render the Counter component here
+  render(<Counter />)
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  expect(screen.getByRole("button", { name: "+" })).toBeInTheDocument()
+  expect(screen.getByRole("button", { name: "-" })).toBeInTheDocument()
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  expect(screen.getByTestId('count')).toHaveTextContent('0')
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const incrementButton = screen.getByRole("button", { name: "+" });
+  fireEvent.click(incrementButton);
+  expect(screen.getByTestId("count")).toHaveTextContent("1");
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const decrementButton = screen.getByRole("button", { name: "-" });
+  fireEvent.click(decrementButton);
+  expect(screen.getByTestId("count")).toHaveTextContent("-1");
 });
